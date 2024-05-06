5c7979acb9aa40a074890b7d7a5160b3501c7f89
KevinJp21 <kevinjp821@gmail.com>
2024-05-06 14:00:14 -0500

    se conectó el chatbot a una bd

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import json
 import random
@@ -9,7 +10,18 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 
 chatbot = Flask(__name__)
-CORS(chatbot)
+chatbot.config['SQLALCHEMY_DATABASE_URI'] = '***REMOVED***'
+chatbot.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(chatbot)
+
+#BD
+
+class User(db.Model):
+    __tablename__ = 'usuarios'
+    ID_Usu = db.Column(db.Integer, primary_key=True)
+    Nombre = db.Column(db.String(255))
+    Apellido = db.Column(db.String(255))
 
 # Carga los modelos y los archivos necesarios
 lemmatizer = WordNetLemmatizer()
@@ -44,20 +56,29 @@ def predict_class(sentence):
     category = classes[max_index]
     return category
 
-def get_response(tag):
-    for i in intents['intents']:
-        if i['tag'] == tag:
-            return random.choice(i['responses'])
+def get_response(tag, user_name):
+    for intent in intents['intents']:
+        if intent['tag'] == tag:
+            # Elegir una respuesta al azar y formatearla con el nombre del usuario
+            response = random.choice(intent['responses'])
+            return response.replace("{name}", user_name)
+        
 
 # Endpoint de la API
 @chatbot.route('/message', methods=['POST'])
 def get_bot_response():
-    sentence = request.json['message']
+    user = User.query.filter_by(ID_Usu=26).first()  # hardcoded for user ID 26
+    if user is None:
+        return jsonify({"response": "Usuario no encontrado para ID 26."})
+    
+    full_name = f"{user.Nombre} {user.Apellido}"
+
+    user_data = request.json
+    sentence = user_data.get('message')
     tag = predict_class(sentence)
-    response = get_response(tag)
+    response = get_response(tag, full_name)
     return jsonify({"response": response})
 
-# Punto de entrada principal
 if __name__ == "__main__":
     from waitress import serve
     serve(chatbot, host="0.0.0.0", port=8080)
